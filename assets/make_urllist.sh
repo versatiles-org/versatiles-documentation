f3e3e18f6e357384b4c3e0b55724fecf587b83b1
Michael Kreil <github@michael-kreil.de>
2023-09-19 09:40:44 +0200

    fix: update script

--- assets/make_urllist.sh
+++ assets/make_urllist.sh
@@ -1,3 +1,6 @@
-# run me in /var/www/download.versatiles.org/docs
 
-echo -e "TsvHttpData-1.0\nhttps://download.versatiles.org/planet-20230605.versatiles\t$(stat -c %s planet-20230605.versatiles)\t$(pv planet-20230605.versatiles | openssl dgst -md5 -binary | openssl enc -base64)\n" > urllist.tsv
+FILE=planet-latest.versatiles
+DIRE=/var/www/download.versatiles.org/docs
+SIZE=$(stat -Lc %s $DIRE/$FILE)
+HASH=$(pv $DIRE/$FILE | openssl dgst -md5 -binary | openssl enc -base64)
+echo -e "TsvHttpData-1.0\nhttps://download.versatiles.org/$FILE\t$SIZE\t$HASH\n" > $DIRE/urllist.tsv
