f6d993cdfea4a057cb6da4c524189953e1938309
Michael Kreil <github@michael-kreil.de>
2026-02-20 18:10:14 +0100

    fix: linting errors

--- .vitepress/theme/components/Showcases.vue
+++ .vitepress/theme/components/Showcases.vue
@@ -47,7 +47,7 @@ function domain(url: string) {
 
 <template>
 	<div class="showcases">
-		<div class="filters" v-if="data.countries.length > 1">
+		<div v-if="data.countries.length > 1" class="filters">
 			<div class="filter-group">
 				<span class="filter-label">Country</span>
 				<button
@@ -86,10 +86,10 @@ function domain(url: string) {
 				rel="noopener noreferrer"
 				class="card"
 			>
-				<div class="card-thumb" v-if="item.thumbnail">
+				<div v-if="item.thumbnail" class="card-thumb">
 					<img :src="item.thumbnail" :alt="item.title" loading="lazy" />
 				</div>
-				<div class="card-thumb placeholder" v-else>
+				<div v-else class="card-thumb placeholder">
 					<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
 						<path
 							d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
